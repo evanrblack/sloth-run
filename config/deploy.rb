dc014201ab08b2fd0f4a69c015b0d7c828bd4992
Evan Black <evan@erblack.com>
2016-06-08 14:05:17 -0500

    Move public folder to shared

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,18 +8,26 @@ set :deploy_to, "~/apps/#{fetch :application}"
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
-append :linked_dirs, 'tmp', 'log'
+append :linked_dirs, 'tmp', 'log', 'public'
 
 namespace :deploy do
   desc 'Make directories for PIDs and Sockets'
-  task :make_dirs do
+  task :make_tmp_dirs do
     on roles(:app) do
       execute "mkdir #{shared_path}/tmp/pids -p"
       execute "mkdir #{shared_path}/tmp/sockets -p"
     end
   end
 
-  before :starting, :make_dirs
+  desc 'Make directory for uploads'
+  task :make_upload_dir do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/public/uploads -p"
+    end
+  end
+
+  before :starting, :make_tmp_dirs
+  before :starting, :make_upload_dir
   after :finishing, 'thin:restart'
 end
 
